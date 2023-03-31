515e9c59330f95bf43997ec91d4c5f7db7c5d5e1
JamariaSims <jamariaxsims@gmail.com>
2023-03-31 13:32:07 -0400

    Added a todo list to the readme

---
+++ TODO.md
@@ -0,0 +1,34 @@
+# JSON DATA STRUCTURE  
+Users:{
+        @username:{
+                username:"",
+                password:"",
+                firstName:"",
+                lastName:"",
+        }
+}
+Posts:{
+        2378025704:{
+                3475375:{
+                        post:"fjhesfhfeo hfowehfi",
+                        createdBy:@Username,
+                        timeStamp:"",
+                        likes:{},
+                        comments:{
+                                23764747:{
+                                        comment:"fdhfdoshf fjefjewei"
+                                        likes:{}
+                                        replyComments:{}
+                                }
+                        }
+                }
+        }
+}
+# Update The Dom
+
+## Append Any Changes
+
+1. Create:
+2. Read:
+3. Update:
+4. Delete:
---
+++ lesson.js
@@ -0,0 +1,59 @@
+let fireBaseURL = "https://connectx-1fd24-default-rtdb.firebaseio.com/";
+let jsonEXT = ".json";
+
+//GET METHOD
+function getData() {
+  fetch(`${fireBaseURL}${jsonEXT}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+getData();
+
+//POST METHOD
+function postData() {
+  fetch(`${fireBaseURL}${jsonEXT}`, {
+    method: "POST",
+    body: JSON.stringify({
+      username: "@JohnDoe",
+      password: "12345",
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+postData();
+
+//DELETE METHOD
+function deleteData(target) {
+  fetch(`${fireBaseURL}${target}/${jsonEXT}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+
+//PATCH METHOD
+function patchData(target) {
+  fetch(`${fireBaseURL}${target}/${jsonEXT}`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      username: "@JohnDoe123",
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+
+//PUT METHOD
+function putData() {
+  fetch(`${fireBaseURL}${jsonEXT}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      somethingUniques: {},
+    }),
+  });
+}
