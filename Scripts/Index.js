3c222d3c786254312b62d2cf685ec35bfe6edd45
JamariaSims <jamariaxsims@gmail.com>
2023-04-03 05:43:27 -0400

    fix api call/login logic/cleaned up code

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -1,37 +1,48 @@
 const form = document.getElementById("Form");
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
-	const username = document.getElementById("username").value;
+	let username = document.getElementById("username").value;
 	const password = document.getElementById("password").value;
 	formValidation(username, password);
 });
 const formValidation = (username, password) => {
-	// If username is less than 7 characters long
-	if (username.length < 7) {
-		console.log("Username must be at least 7 characters long");
+	username = username.toLowerCase();
+	if (containsInvalidChars(username)) {
+		console.log("Username cannot contain spaces or special characters!");
 		return;
 	}
-	// If password is less than 7 characters long
-	else if (password.length < 7) {
-		console.log("Password must be at least 7 characters long");
+	if (isNotCorrectLength(username)) {
+		console.log("Username must be at least 7 characters long");
 		return;
 	}
 	// Next Step - Send off API call
 	attemptLogin(`@${username}`, password);
 };
-
-//Checking if user exist
-async function attemptLogin(username, password) {
-	const response = await fetch(`${fireBaseURL}${username}${jsonEXT}`);
-	const jsonData = (await response.json()) || null;
-	return checkPassword(password, jsonData);
-}
 //Check if password is valid
 function checkPassword(password, data) {
+	// If password is less than 7 characters long
+	if (isNotCorrectLength) {
+		console.log("Password must be at least 7 characters long");
+		return;
+	}
+	//If password contains spaces
+	if (password.contains(/^\s+|\s+$/gm, "")) {
+		console.log("Password cannot contain spaces!");
+		return;
+	}
 	if (!data || password !== data.password) {
 		console.log("Username or password is incorrect!");
 		return;
 	}
 	console.log("Logging In.............");
 	window.location.replace("/Pages/MainFeed.html");
+}
+//Check for white spaces and special characters
+function containsInvalidChars(data) {
+	let format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+	return format.test(data);
+}
+// Check If length is less than 7 characters long
+function isNotCorrectLength(data) {
+	return data.length < 7;
 }
--- Scripts/Utils.js
+++ Scripts/Utils.js
@@ -1,2 +1,9 @@
 let fireBaseURL = "https://connectx-1fd24-default-rtdb.firebaseio.com/";
 let jsonEXT = ".json";
+
+//Checking if user exist
+async function attemptLogin(username, password) {
+	const response = await fetch(`${fireBaseURL}Users/${username}${jsonEXT}`);
+	const jsonData = await response.json();
+	return checkPassword(password, jsonData || "");
+}
