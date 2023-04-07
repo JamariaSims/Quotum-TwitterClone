df50db5b3e9a23016251982634f40c23d532f681
JamariaSims <jamariaxsims@gmail.com>
2023-04-06 22:48:41 -0400

    Login now present ui errors

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -5,16 +5,16 @@ const formData = ["firstName", "lastName", "username", "password"];
 const form = document.getElementById("Form");
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
-	const username = "@" + document.getElementById("username").value;
+	const username =
+		"@" + document.getElementById("username").value.toLowerCase();
 	const password = document.getElementById("password").value;
 	fetch(`${fireBaseURL}Users/${username}${jsonEXT}`)
 		.then((res) => {
 			return res.json();
 		})
 		.then((data) => {
-			console.log(data);
 			if (data.password !== password) {
-				console.log("wrong password!");
+				resetInputValues();
 			}
 			fetch(`${fireBaseURL}currentUser/${jsonEXT}`, {
 				method: "PUT",
@@ -35,5 +35,14 @@ form.addEventListener("submit", (event) => {
 				.then(() => {
 					window.location.replace("/Pages/MainFeed.html");
 				});
+		})
+		.catch((err) => {
+			resetInputValues();
 		});
 });
+
+const resetInputValues = () => {
+	document.getElementById("username").value = "";
+	document.getElementById("password").value = "";
+	return document.getElementById("ERROR-Login").classList.remove("hide");
+};
